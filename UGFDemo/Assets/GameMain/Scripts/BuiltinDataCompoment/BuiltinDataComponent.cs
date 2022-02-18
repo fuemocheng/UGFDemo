3706143206d533796d7332cc2e8cb59776cc8361
fuemocheng <fuemocheng@163.com>
2022-02-18 16:18:48 +0800

    add ProcedureLaunch

---
+++ UGFDemo/Assets/GameMain/Scripts/BuiltinDataCompoment/BuiltinDataComponent.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityGameFramework.Runtime;
+using GameFramework;
+
+namespace UGFDemo
+{
+    public class BuiltinDataComponent : GameFrameworkComponent
+    {
+        [SerializeField]
+        private TextAsset m_BuildInfoTextAsset = null;
+
+        [SerializeField]
+        private TextAsset m_DefaultDictionaryTextAsset = null;
+
+
+        private BuildInfo m_BuildInfo = null;
+
+        public BuildInfo BuildInfo
+        {
+            get
+            {
+                return m_BuildInfo;
+            }
+        }
+
+        public void InitBuildInfo()
+        {
+            if (m_BuildInfoTextAsset == null || string.IsNullOrEmpty(m_BuildInfoTextAsset.text))
+            {
+                Log.Info("Build info can not be found or empty.");
+                return;
+            }
+            m_BuildInfo = Utility.Json.ToObject<BuildInfo>(m_BuildInfoTextAsset.text);
+            if(m_BuildInfo == null)
+            {
+                Log.Warning("Parse build info failure.");
+                return;
+            }
+        }
+
+        
+    }
+
+}
---
+++ UGFDemo/Assets/GameMain/Scripts/Procedure/ProcedureSplash.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameFramework.Procedure;
+using UnityGameFramework.Runtime;
+using GameFramework.Resource;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+
+namespace UGFDemo
+{
+    public class ProcedureSplash : ProcedureBase
+    {
+        public override bool UseNativeDialog
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        protected override void OnInit(ProcedureOwner procedureOwner)
+        {
+            base.OnInit(procedureOwner);
+        }
+
+        protected override void OnEnter(ProcedureOwner procedureOwner)
+        {
+            base.OnEnter(procedureOwner);
+
+        }
+
+        protected override void OnUpdate(ProcedureOwner procedureOwner, float elapseSeconds, float realElapseSeconds)
+        {
+            base.OnUpdate(procedureOwner, elapseSeconds, realElapseSeconds);
+        }
+
+        protected override void OnLeave(ProcedureOwner procedureOwner, bool isShutdown)
+        {
+            base.OnLeave(procedureOwner, isShutdown);
+        }
+
+        protected override void OnDestroy(ProcedureOwner procedureOwner)
+        {
+            base.OnDestroy(procedureOwner);
+        }
+
+    }
+
+}
